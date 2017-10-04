7707314b2728c93747a6623e83dfc963da5ac6c3
AlexanderKing\Alexander King <alexander@akingsquest.com>
2017-10-03 21:57:50 -0400

    Adding a feature so great, you will wonder why you didn't think of it (needs cleaning up, just prelim)

---
+++ GameCenterLogo/Assets/scripts/RendCam.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RendCam : MonoBehaviour {
+
+	Camera actualcamera;
+	public Material newlogomat;
+
+
+	void Start () {
+		actualcamera = GetComponent<Camera>();
+		InitializeCamera();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void InitializeCamera() {
+		
+
+		var tex = new RenderTexture(900, 900, 24, RenderTextureFormat.ARGB32, RenderTextureReadWrite.Default);
+		tex.antiAliasing = 1;
+		tex.wrapMode = TextureWrapMode.Clamp;
+		tex.filterMode = FilterMode.Bilinear;
+		tex.Create();
+		actualcamera.targetTexture = tex;
+		newlogomat.SetTexture("_MainTex", tex);
+		//camera_display.camera_livescreen.texture = tex;
+	}
+}
